1fc689fb97b86f4959ea173a53f418b49d42cbc4
alebarrauc <ale.barra@duocuc.cl>
2024-03-18 01:12:34 -0300

    Cambios realizados

--- src/main/java/com/movies/store/Movie.java
+++ src/main/java/com/movies/store/Movie.java
@@ -3,10 +3,19 @@ package com.movies.store;
 public class Movie {
     private int id;
     private String name;
+    private int year;
+    private String director;
+    private String genre;
+    private String description;
 
-    public Movie(int id, String name){
+
+    public Movie(int id, String name, int year, String director, String genre, String description){
         this.id = id;
         this.name = name;
+        this.year = year;
+        this.director = director;
+        this.genre = genre;
+        this.description = description;
     }
     
     //Getters and setters
@@ -16,6 +25,22 @@ public class Movie {
 
     public String getName() {
         return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getDecription() {
+        return description;
     }
 
 
--- src/main/java/com/movies/store/MovieController.java
+++ src/main/java/com/movies/store/MovieController.java
@@ -1,6 +1,5 @@
 package com.movies.store;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,8 +12,10 @@ public class MovieController {
     private List<Movie> movies = new ArrayList<>();
     
    public MovieController(){
-        movies.add(new Movie(1, "cloud atlas"));
-   }
+        movies.add(new Movie(1, "Cloud Atlas", 2012, "Wachoski Brother","SCI-FI,SPIRITUAL","Set of several stories that take place in the past, present and future."));
+        movies.add(new Movie(2, "Dark City", 1998, "Alex Proyas","SCI-FI,Fantastic, neo-noir","A man struggles with memories of his past."));
+        movies.add(new Movie(3, "The Terminator", 1984, "James Cameron","Terror, Sci-Fi","The movie is about the future battle between the artificial intelligence program Skynet and humanity.."));
+    }
 
 
 @GetMapping("/movies")
